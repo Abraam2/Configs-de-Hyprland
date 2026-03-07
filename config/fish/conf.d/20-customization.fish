e05c2086317e27fd90a42239270bdbcb195740eb
Abraam2 <dragonamarillo25@gmail.com>
2026-03-07 16:48:51 +0100

    Ahora el tema de ohmyposh se cambia con un atajo. Salu2

--- config/fish/conf.d/20-customization.fish
+++ config/fish/conf.d/20-customization.fish
@@ -6,7 +6,7 @@
 # Prompt
 # -----------------------------------------------------
 #eval "$($HOME/.local/bin/oh-my-posh init fish --config $HOME/.config/ohmyposh/zen.toml)"
-eval "$($HOME/.local/bin/oh-my-posh init fish --config $HOME/.config/ohmyposh/EDM115-newline.omp.json)"
+eval "$($HOME/.local/bin/oh-my-posh init fish --config $HOME/.config/ohmyposh/pene.omp.json)"
 
 # Forzar colores de Dracula al inicio
 fish_config theme choose "Dracula Official"
